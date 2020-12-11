ef7fb6b1a7f35ea2f572fbc6e20d0871578310e3
fftl <fftl.0710@gmail.com>
2020-12-11 20:54:24 +0900

    201211, 20:53, templates update

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,5 +41,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/"; //이렇게 하면 첫번째 페이지 / 페이지로 돌아가게 됩니다.
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
